35d85bda09feb7683e0055232ae9b028bec0fda5
Swapnil Gorakhanath Kshirsagar <Swapnil.Kshirsagar@coforge.com>
2024-01-24 17:29:11 +0530

    class type changed to object

--- coforgelib/src/main/java/com/app/coforgelib/SampleToast.kt
+++ coforgelib/src/main/java/com/app/coforgelib/SampleToast.kt
@@ -3,7 +3,7 @@ package com.app.coforgelib
 import android.content.Context
 import android.widget.Toast
 
-class SampleToast {
+object SampleToast {
     fun showSimpleToast(context: Context, msg: String) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
